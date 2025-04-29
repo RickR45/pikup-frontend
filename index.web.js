84519871493d214238e2344cdd5d34352eba4683
RickR45 <rickdr6@gmail.com>
2025-04-29 03:40:29 -0500

    Clean up project for web deployment: remove React Native files and simplify configuration

--- index.web.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('Pikup', () => App);
-AppRegistry.runApplication('Pikup', {
-  initialProps: {},
-  rootTag: document.getElementById('root')
-}); 
