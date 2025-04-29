65a24e52fae1ceb0aaf2c84f832598e9531ef9f1
RickR45 <rickdr6@gmail.com>
2025-04-29 03:45:30 -0500

    Remove babel-plugin-module-resolver and clean dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module-resolver', {
-      root: ['./src'],
-      extensions: [
-        '.ios.ts',
-        '.android.ts',
-        '.ts',
-        '.ios.tsx',
-        '.android.tsx',
-        '.tsx',
-        '.jsx',
-        '.js',
-        '.json',
-      ],
-      alias: {
-        '@components': './src/components',
-        '@screens': './src/screens',
-        '@assets': './src/assets',
-      },
-    }],
-  ],
+  presets: ['react-app'],
+  plugins: []
 }; 
